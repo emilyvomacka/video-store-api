716750b755f73c30825fb9a94f7876a1ceead915
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-05 20:31:50 -0800

    movies_ctrller test codes skeleton

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "INDEX" do
+    it "basic test w/ no parameters, if applicable" do
+    end
+
+    it "nominal case: check status and request body" do
+    end
+
+    it "edge case: check status and request body" do
+    end
+  end
+
+  describe "SHOW" do
+    it "basic test w/ no parameters, if applicable" do
+    end
+
+    it "nominal case: check status and request body" do
+    end
+
+    it "edge case: check status and request body" do
+    end
+  end
+
+  describe "CREATE" do
+    it "basic test w/ no parameters, if applicable" do
+    end
+
+    it "nominal case: check status and request body" do
+    end
+
+    it "edge case: check status and request body" do
+    end
+  end
 end
