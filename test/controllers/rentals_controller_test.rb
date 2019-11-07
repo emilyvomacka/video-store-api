faec3a3c476f7cf77dd7cce99f81d523ff94abf3
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-06 20:53:39 -0800

    setting up rentals#check_out skeleton

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "CHECK_OUT" do
+    
+    describe "successful check_out with valid inputs" do
+      it "creates Rental instance with correct attribs" do
+      end
+      
+      it "updates movie's available_inventory correctly" do
+      end
+      
+      it "updates customer's movies_checked_out_count correctly" do
+      end
+      
+      it "returns JSON with success status" do
+      end
+      
+    end
+    
+    describe "edge" do
+      it "if movie's available_inventory = 0, return error msg in JSON and bad_request status" do
+      end
+      
+      
+    end
+    
+  end
+  
+  describe "CHECK_IN" do
+    
+    describe "nominal" do
+    end
+    
+    describe "edge" do
+    end
+    
+  end
 end
