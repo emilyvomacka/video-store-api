7236314bab69dd62bf7c0391e85269f2da975570
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-07 14:23:13 -0800

    sorting by name/registered_at/postal_code works for Customers#index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,6 +4,18 @@ class CustomersController < ApplicationController
   
   def index
     customers = Customer.all 
+    
+    if ["name", "registered_at", "postal_code"].include? params[:sort]
+      choice = params[:sort].to_sym
+      customers = Customer.all.order(choice => :asc)
+    end
+    
+    if params[:n] && params[:p]
+      # I think these 2 query params are used together? otherwise p by itself makes no sense
+      # TODO
+    end
+    
     render json: customers.as_json( only: CUSTOMER_KEYS), status: :ok
   end 
+  
 end
