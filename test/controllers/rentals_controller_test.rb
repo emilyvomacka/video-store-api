79f4572beaf162235b27ce1f87a71bfa769bbcfb
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-06 21:40:32 -0800

    trying to fix bugs in check_out test codes

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -3,25 +3,77 @@ require "test_helper"
 describe RentalsController do
   describe "CHECK_OUT" do
     
+    let (:m1) { movies(:m1) }
+    let (:c3) { customers(:c3) }
+    
     describe "successful check_out with valid inputs" do
+      
+      before do
+        # is this better than having a huge IT block?
+        @rental_count_before = Rental.count
+        @m1_avail_inv_before = m1.available_inventory
+        @c3_movie_count_before = c3.movies_checked_out_count
+        puts "\nBEFORE"
+        p m1
+        p c3
+        post check_out_path, params: {movie_id: m1.id, customer_id: c3.id}
+        
+        m1B = Movie.find_by(id: m1.id)
+        c3B = Customer.find_by(id: c3.id)
+        puts "AFTER"
+        p m1B
+        p c3B
+        puts
+      end
+      
       it "creates Rental instance with correct attribs" do
+        expect(Rental.count).must_equal @rental_count_before+1
+        
+        rental = Rental.last
+        expect(rental.movie).must_equal m1
+        expect(rental.customer).must_equal c3
+        expect(rental.checkout_date).must_equal Date.today
+        expect(rental.due_date).must_equal (Date.today + 7.days)
+        expect(rental.returned).must_equal false
       end
       
       it "updates movie's available_inventory correctly" do
+        expect(m1.available_inventory).must_equal m1.inventory - 1
+        expect(m1.available_inventory).must_equal @m1_avail_inv_before - 1
       end
       
       it "updates customer's movies_checked_out_count correctly" do
+        expect(c3.movies_checked_out_count).must_equal @c3_movie_count_before + 1
       end
       
-      it "returns JSON with success status" do
+      it "returns success status & JSON with expected message" do
+        check_response(expected_type: Hash, expected_status: :success)
+        
+        body = JSON.parse(response.body)
+        expect(body.keys).must_equal ["msg"]
+        expect(body.values).must_equal ["Rental id #{Rental.last.id}: #{m1.title} due on #{Rental.last.due_date}"]
       end
       
     end
     
     describe "edge" do
       it "if movie's available_inventory = 0, return error msg in JSON and bad_request status" do
+        m1.update!(available_inventory: 0)
+        
+        expect{post check_out_path, params: {movie_id: m1.id, customer_id: c3.id}}.wont_change "Rental.count"
+        check_response(expected_type: Hash, expected_status: :bad_request)
+        
+        body = JSON.parse(response.body)
+        expect(body.keys).must_equal ["errors"]
+        expect(body.values).must_equal ["Cannot make a rental because movie #{m1.title.capitalize} ran out of copies"]
       end
       
+      it "if rental.save unsuccessful, return error msg in JSON and bad_request status" do
+        
+        
+        # render json: { errors: "Cannot make a rental", error_msgs: new_rental.errors.full_messages }, status: :bad_request
+        
+      end
       
     end
     
@@ -33,6 +85,8 @@ describe RentalsController do
     end
     
     describe "edge" do
+      it "if movie has already been returned" do
+      end
     end
     
   end
