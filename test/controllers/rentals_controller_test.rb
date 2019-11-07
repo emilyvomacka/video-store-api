d8e4489364a50da33bb185e97012a104eaf01227
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-07 10:41:55 -0800

    fixed yml, thereby fixing test codes, yay

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'pry'
 
 describe RentalsController do
   describe "CHECK_OUT" do
@@ -9,14 +10,11 @@ describe RentalsController do
     describe "successful check_out with valid inputs" do
       
       before do
-        # is this better than having a huge IT block?
         @rental_count_before = Rental.count
         @m1_avail_inv_before = m1.available_inventory
         @c3_movie_count_before = c3.movies_checked_out_count
+
         post check_out_path, params: {movie_id: m1.id, customer_id: c3.id}
-        
-        @m1_avail_inv_after = (Movie.find_by(id: m1.id)).available_inventory
-        @c3_movie_count_after = (Customer.find_by(id: c3.id)).movies_checked_out_count
       end
       
       it "creates Rental instance with correct attribs" do
@@ -31,10 +29,12 @@ describe RentalsController do
       end
       
       it "updates movie's available_inventory correctly" do
+        @m1_avail_inv_after = (Movie.find_by(id: m1.id)).available_inventory
         expect(@m1_avail_inv_after).must_equal @m1_avail_inv_before - 1
       end
       
       it "updates customer's movies_checked_out_count correctly" do
+        @c3_movie_count_after = (Customer.find_by(id: c3.id)).movies_checked_out_count
         ### WHY???!!! Postman still passed! I also manually tested in rails console!
         expect(@c3_movie_count_after).must_equal @c3_movie_count_before + 1
       end
