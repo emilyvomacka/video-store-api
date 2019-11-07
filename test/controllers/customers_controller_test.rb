ea741b4825f9dbf55da59dbbbb918348cd9463e6
Emily Vomacka <emilyvomacka@gmail.com>
2019-11-07 09:55:29 -0800

    test_helper

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,12 +1,29 @@
 require "test_helper"
 
+CUSTOMER_FIELDS = %w(id name phone postal_code registered_at movies_checked_out_count).sort
+
 describe CustomersController do
   
   describe "INDEX" do
-    it "basic test w/ no parameters, if applicable" do
+    it "responds with JSON, success, and an array of customer hashes" do
+      get customers_path
+      body = check_response(expected_type: Array)
+
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+      end 
+    end
+
+    it "responds with an empty array when there are no customers" do
+      Customer.destroy_all
+      get customers_path
+      body = check_response(expected_type: Array)
+      expect(body).must_be_empty
     end
     
-    it "nominal case: check status and request body" do
+    it "displays list of customers" do
+      
     end
     
     it "edge case: check status and request body" do
