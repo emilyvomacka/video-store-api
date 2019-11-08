747d634ba0020bb202380ac77d6129194db74ff1
Emily Vomacka <emilyvomacka@gmail.com>
2019-11-07 21:22:10 -0800

    movies controller test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,58 +1,58 @@
 require "test_helper"
 
-MOVIE_KEYS = (%w[id name postal_code phone registered_at movies_checked_out_count]).sort
+MOVIE_SHOW_KEYS = (%w[id name postal_code phone registered_at movies_checked_out_count]).sort
+MOVIE_INDEX_KEYS = (%w[id title release_date]).sort
 
 describe MoviesController do
   describe "INDEX" do
-    describe "INDEX" do
-      it "responds with JSON and success status" do
-        get movies_path
-        check_response(expected_type: Array)
-      end
-      
-      it "if movies exist, responds with an array of movie hashes" do
-        get movies_path
-        
-        body = JSON.parse(response.body)
-        
-        expect(body.count).must_equal Movie.count
-        
-        body.each do |movie|
-          expect(movie).must_be_instance_of Hash
-          expect(movie.keys.sort).must_equal CUSTOMER_KEYS
-        end
-        
-      end
-      
-      it "if no movies exist, responds with check status and request body" do
-        Movie.destroy_all
-        
-        get movies_path
-        
-        body = JSON.parse(response.body)
-        expect(body.count).must_equal Movie.count
-        expect(body).must_equal []
-      end
+    it "responds with JSON and success status" do
+      get movies_path
+      check_response(expected_type: Array)
     end
-
+    
+    it "if movies exist, responds with an array of movie hashes" do
+      get movies_path
+      
+      body = JSON.parse(response.body)
+      
+      expect(body.count).must_equal Movie.count
+      
+      body.each do |movie|
+        expect(movie).must_be_instance_of Hash
+        expect(movie.keys.sort).must_equal MOVIE_INDEX_KEYS
+      end
+      
+    end
+    
+    it "if no movies exist, responds with check status and request body" do
+      Movie.destroy_all
+      
+      get movies_path
+      
+      body = JSON.parse(response.body)
+      expect(body.count).must_equal Movie.count
+      expect(body).must_equal []
+    end
+  end
+  
   describe "SHOW" do
     it "basic test w/ no parameters, if applicable" do
     end
-
+    
     it "nominal case: check status and request body" do
     end
-
+    
     it "edge case: check status and request body" do
     end
   end
-
+  
   describe "CREATE" do
     it "basic test w/ no parameters, if applicable" do
     end
-
+    
     it "nominal case: check status and request body" do
     end
-
+    
     it "edge case: check status and request body" do
     end
   end
